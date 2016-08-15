9c82c02f5b46742112cd22f035c6285fb06ad95d
youngkook <youngkook1992@163.com>
2016-08-15 17:13:27 +0800

    Initial Commit

---
+++ Algorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Algorithm
+//
+//  Created by Youngkook on 16/8/15.
+//  Copyright © 2016年 youngkook. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
